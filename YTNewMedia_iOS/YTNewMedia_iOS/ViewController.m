74eda05aeeede27cbc8f638bc709cc1e580ca6b3
jiahong <qu_jia_hong@163.com>
2018-02-01 15:48:21 +0800

    create product

---
+++ YTNewMedia_iOS/YTNewMedia_iOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YTNewMedia_iOS
+//
+//  Created by qujiahong on 2018/2/1.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ YTNewMedia_iOS/YTNewMedia_iOSTests/YTNewMedia_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  YTNewMedia_iOSTests.m
+//  YTNewMedia_iOSTests
+//
+//  Created by qujiahong on 2018/2/1.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YTNewMedia_iOSTests : XCTestCase
+
+@end
+
+@implementation YTNewMedia_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
